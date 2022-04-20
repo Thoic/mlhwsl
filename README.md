d9a2a94e86e27c41a3e09c9e5a8107eb29b7ec1e
Nick <nicholas.j.thompson-1@ou.edu>
2022-04-20 09:24:18 -0500

    use dot product instead of sum

---
+++ README.md
@@ -0,0 +1,3 @@
+# Machine Learning Project 2
+Nick Thompson - 4033
+Eric Chen - 4033
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression, LogisticRegression
@@ -7,50 +8,68 @@ input_file = 'data/winequality-red.csv'
 # perceptron learning rate
 ETA = 0.1
 
-def output(w, x):
-    if (sum(w[i]*x[i] for i in range(len(w)))) > 0:
+def output(w: np.ndarray, x: np.ndarray):
+    if (w[0] + np.dot(x.T, w[1:])) > 0:
         return 1
     else:
         return -1
 
 
 def perceptronUpdate(w, t, o, x):
-    for i in range(len(w)):
-        w[i] += ETA * (t - o) * x[i]
+    w[0] += ETA * (t - o)
+    for i in range(1, len(w)):
+        w[i] += ETA * (t - o) * x[i-1]
 
-def Perceptron(X_train: pd.DataFrame, y_train, X_test: pd.DataFrame, y_test):
+def Perceptron(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame):
     # modify train/test set for binary classification
     y_train[:] = [1 if item >= 6 else -1 for item in y_train]
     y_test[:] = [1 if item >= 6 else -1 for item in y_test]
 
-    X_train.insert(0, 'convenience', 1)
-    X_test.insert(0, 'convenience', 1)
-
-    w = np.zeros(len(X_train.to_numpy()[0]))
+    w = np.zeros(len(X_train.to_numpy()[0]) + 1)
 
     w_prev = w.copy()
-    for j in range(1, 1000):
-        for idx, item in enumerate(y_train):
+    for j in range(1, 501):
+        for x, t in zip(X_train.to_numpy(), y_train.to_numpy()):
             # x vector with x[0] = 1
-            x = X_train.iloc[idx]
-            t = item
             o = output(w, x)
             perceptronUpdate(w, t, o, x)
 
         print(f'after the {j} iteration')
         print(f'w: {w}')
-        if (w == w_prev).all():
+        isSolved = True
+        for weight, prev_weight in zip(w, w_prev):
+            if not math.isclose(weight, prev_weight):
+                isSolved = False
+                break
+        if isSolved:
             break
         else:
             w_prev = w.copy()
     
     y_predict = []
-    for idx, item in enumerate(X_test.to_numpy()):
+    for item in X_test.iloc:
         y_predict.append(output(w, item))
     
     confuse_matrix = pd.crosstab(y_test, y_predict, rownames=['Actual'], colnames=['Predicted'])
     print(confuse_matrix)
     
+
+def LinReg(X_train, y_train, X_test, y_test):
+    # linear regression
+    reg = LinearRegression().fit(X_train, y_train)
+
+    predict = reg.predict(X_test)
+    print(predict)
+
+    confuse_matrix = pd.crosstab(y_test, predict, rownames=['Actual'], colnames=['Predicted'])
+
+def LogReg(X_train, y_train, X_test, y_test):
+    reg = LogisticRegression().fit(X_train, y_train)
+
+    predict = reg.predict(X_test)
+
+    confuse_matrix = pd.crosstab(y_test, predict, rownames=['Actual'], colnames=['Predicted'])
+    print(confuse_matrix)
     
 
 def main():

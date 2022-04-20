b7e639442e0f288cf9e93ef40dc79582ae16ef8b
Nick <nicholas.j.thompson-1@ou.edu>
2022-04-20 10:40:49 -0500

    re-add convenience column

--- main.py
+++ main.py
@@ -9,25 +9,28 @@ input_file = 'data/winequality-red.csv'
 ETA = 0.1
 
 def output(w: np.ndarray, x: np.ndarray):
-    if (w[0] + np.dot(x.T, w[1:])) > 0:
+    if (np.dot(w, x.T)) > 0:
         return 1
     else:
         return -1
 
 
 def perceptronUpdate(w, t, o, x):
-    w[0] += ETA * (t - o)
-    for i in range(1, len(w)):
-        w[i] += ETA * (t - o) * x[i-1]
+    for i in range(len(w)):
+        w[i] += ETA * (t - o) * x[i]
 
 def Perceptron(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFrame, y_test: pd.DataFrame):
 
-    w = np.zeros(len(X_train.to_numpy()[0]) + 1)
+    X_train.insert(0, 'convenience', 1)
+    X_test.insert(0, 'convenience', 1)
+
+    print(X_train)
+
+    w = np.zeros(len(X_train.to_numpy()[0]))
 
     w_prev = w.copy()
-    for j in range(1, 501):
+    for j in range(1, 1001):
         for x, t in zip(X_train.to_numpy(), y_train.to_numpy()):
-            # x vector with x[0] = 1
             o = output(w, x)
             perceptronUpdate(w, t, o, x)
 
@@ -35,7 +38,7 @@ def Perceptron(X_train: pd.DataFrame, y_train: pd.DataFrame, X_test: pd.DataFram
         print(f'w: {w}')
         isSolved = True
         for weight, prev_weight in zip(w, w_prev):
-            if not math.isclose(weight, prev_weight):
+            if not math.isclose(weight, prev_weight, abs_tol=0.001):
                 isSolved = False
                 break
         if isSolved:
@@ -84,8 +87,8 @@ def main():
     y_test[:] = [1 if item >= 6 else -1 for item in y_test]
 
     # LinReg(X_train, y_train, X_test, y_test)
-    LogReg(X_train, y_train, X_test, y_test)
-    # Perceptron(X_train, y_train, X_test, y_test)
+    # LogReg(X_train, y_train, X_test, y_test)
+    Perceptron(X_train, y_train, X_test, y_test)
 
 if __name__ == "__main__":
     main()
